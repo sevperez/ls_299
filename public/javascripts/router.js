1bef34aa2c28daa4bc157fcb2fbfd05c8a53acc5
Severin Perez <severinperez@gmail.com>
2017-12-22 13:31:59 +0000

    initial router setup

---
+++ public/javascripts/router.js
@@ -0,0 +1,25 @@
+// BACKBONE - ROUTER
+
+App.router = new (Backbone.Router.extend({
+  routes: {
+    
+  },
+  
+  initialize: function() {
+    // create index route, using regex (avoid '/' requirement on routes object)
+    this.route(/^\/?$/, "index", this.index);
+  },
+}))();
+
+// start Backbone history
+Backbone.history.start({
+  pushState: true
+});
+
+// listen for any anchor clicks with a root path and defer to router
+$(document).on("click", "a[href^='/']", function(e) {
+  e.preventDefault();
+  
+  // adjust href property to remove forward slash; pass to the router to handle
+  App.router.navigate($(e.currentTarget).attr("href").replace(/^\//, ""), { trigger: true });
+});
